9be745c2fcdf93094cdb1cc82d79b1656e86df66
Aayush Gupta <cdtaayushgupta@gmail.com>
2023-06-27 01:06:24 +0530

    Added header authorisation middleware

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,2 +1,17 @@
+import { Request, Response, NextFunction } from 'express';
+import { unauthAccess } from '../views/view';
+import jwt from 'jsonwebtoken';
 
-// export function authMiddleWare
+export function authMiddleWare(req: Request, res: Response, next: NextFunction) {
+    const authrisationToken = req.headers.authorization;
+    if (!authrisationToken) {
+        unauthAccess(res);
+        return;
+    }
+    const jwtToken = authrisationToken.split(' ')[1];
+    if (!jwt.verify(jwtToken, (process.env.SECRET_KEY as string))) {
+        unauthAccess(res);
+        return;
+    }
+    next();
+}
