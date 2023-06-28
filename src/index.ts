30de5d0959fdf3a80b790b86745ae083005d876f
Aayush Gupta <cdtaayushgupta@gmail.com>
2023-06-29 04:09:20 +0530

    Simple ping endpoint added

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import { config } from 'dotenv';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import { Request, Response } from 'express';
 import { loginUsersController, registerUsersController } from './controllers/AuthControllers';
 import { authMiddleWare } from './middleware/AuthMiddleware';
 import { getGrievancesController, postGrievancesController } from './controllers/GrievancesControllers';
 import { issueToken } from './controllers/AccessTokenIssueController';
+import { statusOkay } from './views/view';
 
 
 config();
@@ -16,15 +18,18 @@ app.use(cors());
 app.get('/accesstoken', issueToken);
 app.post('/register', registerUsersController);
 app.post('/login', loginUsersController);
+app.get('/ping', (req: Request, res: Response) => statusOkay(res, {message: "Server Running"}));
 
 app.use(authMiddleWare);
 app.get('/grievances/:regno', getGrievancesController);
 app.post('/grievances', postGrievancesController);
 
 mongoose.connect(process.env.MONGO_URL!).then(() => {
+    console.clear();
     console.log(`Connected to MongoDB and Listening on Port ${process.env.PORT}`);
     app.listen(process.env.PORT);
 }).catch((err) => {
+    console.clear();
     console.log("Can't connect to the MongoDB");
     console.log(err);
 })
