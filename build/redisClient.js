409d2fdbb8c7b6fe843630a0300575bbc09ced02
Aayush Gupta <cdtaayushgupta@gmail.com>
2023-07-08 19:56:02 +0530

    Changing redisClient acc to env

--- build/redisClient.js
+++ build/redisClient.js
@@ -4,4 +4,12 @@ exports.redisClient = void 0;
 const redis_1 = require("redis");
 const dotenv_1 = require("dotenv");
 (0, dotenv_1.config)();
-exports.redisClient = (0, redis_1.createClient)({ url: process.env.REDIS_URL });
+const options = {
+    socket: {
+        host: process.env.REDIS_HOST,
+        port: Number(process.env.REDIS_PORT)
+    },
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD
+};
+exports.redisClient = (0, redis_1.createClient)(options);
--- src/redisClient.ts
+++ src/redisClient.ts
@@ -2,4 +2,14 @@ import { createClient } from 'redis';
 import { config } from 'dotenv';
 config();
 
-export const redisClient = createClient({ url: process.env.REDIS_URL });
+
+const options = {
+    socket: {
+        host: process.env.REDIS_HOST,
+        port: Number(process.env.REDIS_PORT)
+    }, 
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD
+}
+
+export const redisClient = createClient(options);
